89f4d14f9ecaa38e1a931423d7c4cb7166b30fbd
huangchangjun <421536376@qq.com>
2026-06-15 02:39:44 +0800

    perf(demo): delete demo code

--- routes/router.go
+++ routes/router.go
@@ -13,30 +13,16 @@ import (
 // SetupRoutes 设置所有路由
 func SetupRoutes(r *gin.Engine) error {
 	api := r.Group("/api")
-	departmentRegisterRoutes(api)
 	if err := fileRegisterRoutes(api); err != nil {
 		return err
 	}
 	if err := chatRegisterRoutes(api); err != nil {
 		return err
 	}
-	userRegisterRoutes(api)
-	return nil
-}
-
-// demo 注册部门相关的路由
-func departmentRegisterRoutes(api *gin.RouterGroup) {
-	departmentHandler := handlers.NewDepartmentHandler()
-	{
-		Departments := api.Group("/department")
-		{
-			Departments.GET("", departmentHandler.GetDepartmentList)       // 列表查询
-			Departments.POST("", departmentHandler.CreateDepartment)       // 创建
-			Departments.GET("/:id", departmentHandler.GetDepartment)       // 详情
-			Departments.PUT("/:id", departmentHandler.UpdateDepartment)    // 更新
-			Departments.DELETE("/:id", departmentHandler.DeleteDepartment) // 删除
-		}
+	if err := userRegisterRoutes(api); err != nil {
+		return err
 	}
+	return nil
 }
 
 // 文件相关的路由
@@ -92,7 +78,7 @@ func chatRegisterRoutes(api *gin.RouterGroup) error {
 }
 
 // 用户管理相关路由
-func userRegisterRoutes(api *gin.RouterGroup) {
+func userRegisterRoutes(api *gin.RouterGroup) error {
 	userHandler := handlers.NewUserHandler()
 	auth := api.Group("/auth")
 	{
@@ -102,4 +88,5 @@ func userRegisterRoutes(api *gin.RouterGroup) {
 		auth.POST("/check", userHandler.Check)               // 校验会话是否有效
 		auth.POST("/logout", userHandler.Logout)             // 注销会话
 	}
+	return nil
 }
